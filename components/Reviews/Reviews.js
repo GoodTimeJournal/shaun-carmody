ca5fc164a8b3e3d48d11a0938605f5fda1f6f557
c87 <shaun.carmody@live.co.uk>
2019-02-05 14:54:51 +0000

    update stylingfor mobile

---
+++ components/Reviews/Reviews.js
@@ -0,0 +1,28 @@
+class Tab {
+    constructor(props) {
+        this.element = props;
+        this.tab = props.dataset.tab;
+        this.review = document.querySelector(`.review[data-review='${this.tab}']`);
+        console.log(this.review.querySelector('h6').textContent.length);
+        this.element.addEventListener('click', e => this.display());
+    }
+
+    hide() {
+        document.querySelector('.active').classList.toggle('active');
+    }
+
+    display() {
+        this.hide();
+        this.review.classList.toggle('active');
+    }
+
+} 
+
+class Review {
+    constructor(props) {
+        this.element = props;
+    }
+}
+
+document.querySelectorAll('.tab').forEach(el => new Tab(el));
+document.querySelectorAll('.review').forEach(el => new Review(el));
